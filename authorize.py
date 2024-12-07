d1002d64938194900b07ede6825e61854595e8c0
Hector Sanchez <sanhe@m-v-t.com>
2024-12-07 14:00:47 -0700

    Updated authorize.py to use environment variables for Google credentials

--- authorize.py
+++ authorize.py
@@ -1,7 +1,10 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 import pickle
 import os.path
+import json
+import os
 
 # Include both Drive and Gmail scopes
 SCOPES = [
@@ -19,8 +22,15 @@ def get_credentials():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            # Read credentials from environment variable
+            creds_json = os.environ.get('GOOGLE_CREDENTIALS')
+            if not creds_json:
+                raise ValueError("GOOGLE_CREDENTIALS environment variable not found")
+            
+            creds_data = json.loads(creds_json)
+            flow = InstalledAppFlow.from_client_config(creds_data, SCOPES)
             creds = flow.run_local_server(port=0)
+        
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
     return creds
